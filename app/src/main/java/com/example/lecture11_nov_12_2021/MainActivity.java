0ba50e618899d566e4987d3215d686f89cf7e85d
mabbas <mabbasctn69@gmail.com>
2021-11-15 10:08:25 +0500

    Custom toast created

--- app/src/main/java/com/example/lecture11_nov_12_2021/MainActivity.java
+++ app/src/main/java/com/example/lecture11_nov_12_2021/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.lecture11_nov_12_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast toast= Toast.makeText(this,"First toast created", Toast.LENGTH_LONG);
-        toast.show();
+        LayoutInflater inflater= getLayoutInflater();
+        View my_layout= inflater.inflate(R.layout.my_layout,(ViewGroup)findViewById(R.id.dialog_container));
+        ImageView camImage= my_layout.findViewById(R.id.imageView);
+        camImage.setImageResource(R.drawable.ic_launcher_background);
+        TextView msg= (TextView) my_layout.findViewById(R.id.textView);
+        msg.setText("Custom toast created");
+        Toast t= new Toast(getApplicationContext());
+        t.setDuration(Toast.LENGTH_LONG);
+        t.setView(my_layout);
+        t.show();
 
     }
 }
